8c3c25830dd7e0aa50f6f5d638c495f826abe8eb
Slade <128598022+SladeGovender@users.noreply.github.com>
2023-11-22 16:24:16 +0200

    TestTotalSales.java

--- TestTotalSales.java
+++ TestTotalSales.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class TestEstateAgent {
+public class TestTotalSales {
 
     @Test
     public void calculateTotalSales_ReturnsTotalSales() {
