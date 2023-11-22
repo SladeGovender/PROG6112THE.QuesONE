11e97520df3c48a7023b39d0ae8d4290b7f7bb3d
Slade <128598022+SladeGovender@users.noreply.github.com>
2023-11-22 16:27:39 +0200

    TestCommision.java

--- TestCommision.java
+++ TestCommision.java
@@ -2,7 +2,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestCommision {
-  
+
+@test
 public void calculateTotalCommission_ReturnsCommission() {
         // Arrange
         double[] propertySales = {800000, 1500000, 2000000};
