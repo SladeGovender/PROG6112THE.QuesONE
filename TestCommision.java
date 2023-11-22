bb778d7f170cffd47719f85c327db6ad16ca856b
Slade <128598022+SladeGovender@users.noreply.github.com>
2023-11-22 16:26:59 +0200

    TestCommision.java

---
+++ TestCommision.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestCommision {
+  
+public void calculateTotalCommission_ReturnsCommission() {
+        // Arrange
+        double[] propertySales = {800000, 1500000, 2000000};
+        EstateAgent joeBloggs = new EstateAgent("Joe Bloggs", propertySales);
+
+        // Act
+        double commission = joeBloggs.getEstateAgentCommission();
+
+        // Assert
+        assertEquals(86000, commission, 0.01); // Considering double precision
+    }
+  }
